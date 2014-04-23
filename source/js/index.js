c7ce893fe542aa2e2aed55c912c5f8e178bdd0f9
ntalbs <ntalbsen@gmail.com>
2014-04-23 20:09:46 +0900

    casper theme을 기반으로 조금 수정해 기본 틀을 만들었음. 아직 수정해야 할 부분이 많음.

---
+++ source/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Main JS file
+ */
+
+/*globals jQuery, document */
+(function ($) {
+  "use strict";
+
+  $(document).ready(function(){
+    $(".post-content").fitVids();
+  });
+}(jQuery));
