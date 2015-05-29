931bbbd367e092a5fdc699879cea8513a1123d09
ntalbs <ntalbsen@gmail.com>
2015-05-29 12:56:45 +0900

    얼마나 읽었는지 볼 수 있는 진도표시바 추가.

--- source/js/index.js
+++ source/js/index.js
@@ -6,7 +6,16 @@
 (function ($) {
   "use strict";
 
-  $(document).ready(function(){
+  $(document).ready(function (){
     $(".post-content").fitVids();
+  });
+
+  $(document).scroll(function () {
+    var dh = $('.post-content').height(), // document height
+        wh = $(window).height(),          // window height
+        offsetTop = $('.post-content').offset().top,
+        y = $(document.body).scrollTop(),
+        progress = Math.min(100, y / (offsetTop + dh - wh) * 100);
+    $('#bar').css({width: progress + '%'});
   });
 }(jQuery));
