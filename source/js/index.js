c13c8f3efe5e0eac5025f8932a93f9207e8d3e9d
ntalbs <ntalbsen@gmail.com>
2015-06-08 09:35:28 +0900

    스크롤 시 제목 보이는 위치 조정.

--- source/js/index.js
+++ source/js/index.js
@@ -9,6 +9,7 @@
     $(".post-content").fitVids();
   });
 
+  var threshold = $('h1').offset().top + $('h1').height() - $('nav').height();
   $(document).scroll(function () {
     var dh = $('.post-content').height(), // document height
         wh = $(window).height(),          // window height
@@ -18,7 +19,7 @@
         progress = Math.min(100, y / base * 100);
     $('#bar').css({width: progress + '%'});
 
-    if (y <= $('h1').offset().top) {
+    if (y <= threshold) {
       $('#progress').height('8px');
     } else {
       $('#progress').height('36px');
