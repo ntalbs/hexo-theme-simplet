e394158263f42b6b49367e733012fd327d9db290
ntalbs <ntalbsen@gmail.com>
2015-07-28 09:34:07 +0900

    archives 페이지에서 스크립트 오류 발생하는 문제 수정.

--- source/js/index.js
+++ source/js/index.js
@@ -9,24 +9,31 @@
     $(".post-content").fitVids();
   });
 
-  var threshold = $('h1').offset().top + $('h1').height() - $('nav').height(),
-      $postContent = $('.post-content'),
-      ph = $postContent.height(),       // post height
-      wh = $(window).height();          // window height
+  function initProgress ($h1) {
+    var threshold = $h1.offset().top + $h1.height() - $('nav').height(),
+        $postContent = $('.post-content'),
+        ph = $postContent.height(),       // post height
+        wh = $(window).height();          // window height
 
-  $(document).scroll(function () {
-    if (!$postContent.length) return;
+    $(document).scroll(function () {
+      if (!$postContent.length) return;
 
-    var offsetTop = $postContent.offset().top,
-        y = $(document).scrollTop(),
-        base = Math.max(5, offsetTop + ph - wh),
-        progress = Math.min(100, y / base * 100);
-    $('#bar').css({width: progress + '%'});
+      var offsetTop = $postContent.offset().top,
+          y = $(document).scrollTop(),
+          base = Math.max(5, offsetTop + ph - wh),
+          progress = Math.min(100, y / base * 100);
+      $('#bar').css({width: progress + '%'});
 
-    if (y <= threshold) {
-      $('#progress').height('8px');
-    } else {
-      $('#progress').height('36px');
-    }
-  });
+      if (y <= threshold) {
+        $('#progress').height('8px');
+      } else {
+        $('#progress').height('36px');
+      }
+    });
+  }
+
+  var $h1 = $('h1');
+  if ($h1.length) {
+    initProgress($h1);
+  }
 }(jQuery));
