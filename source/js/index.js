02ae9bfc1474fa0fd3712c6d0f1a1134c827e61c
ntalbs <ntalbsen@gmail.com>
2016-10-15 18:01:31 +0100

    페이지 로딩할 때마다 프로그레스 바 색상 랜덤 지정

--- source/js/index.js
+++ source/js/index.js
@@ -9,11 +9,21 @@
     $(".post-content").fitVids();
   });
 
+  function randomColor () {
+    var colors = [
+      '#990099', '#660066', '#330033', '#330066', '#330099', '#003366',
+      '#003399', '#000033', '#333300', '#663300', '#330000', '#990033',
+      '#660033', '#089378', '#0898b3', '#1A5276', '#ffca00', '#ff9900'
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
   function initProgress ($h1) {
     var threshold = $h1.offset().top + $h1.height() - $('nav').height(),
         $postContent = $('.post-content'),
         ph = $postContent.height(),       // post height
-        wh = $(window).height();          // window height
+        wh = $(window).height(),          // window height
+        color = randomColor();
 
     $(document).scroll(function () {
       if (!$postContent.length) return;
@@ -22,7 +32,10 @@
           y = $(document).scrollTop(),
           base = Math.max(5, offsetTop + ph - wh),
           progress = Math.min(100, y / base * 100);
-      $('#bar').css({width: progress + '%'});
+      $('#bar').css({
+        'width': progress + '%',
+        'background-color': color
+      });
 
       if (y <= threshold) {
         $('#progress').height('8px');
