8e20c8ff493df2ce43254db82321c746f28c489d
ntalbs <ntalbsen@gmail.com>
2016-10-15 18:10:05 +0100

    standard 스타일 적용

--- source/js/index.js
+++ source/js/index.js
@@ -3,55 +3,56 @@
  */
 
 (function ($) {
-  "use strict";
+  'use strict'
 
-  $(document).ready(function (){
-    $(".post-content").fitVids();
-  });
+  $(document).ready(function () {
+    $('.post-content').fitVids()
+  })
 
   function randomColor () {
     var colors = [
       '#990099', '#660066', '#330033', '#330066', '#330099', '#003366',
       '#003399', '#000033', '#333300', '#663300', '#330000', '#990033',
       '#660033', '#089378', '#0898b3', '#1A5276', '#ffca00', '#ff9900'
-    ];
-    return colors[Math.floor(Math.random() * colors.length)];
+    ]
+    return colors[Math.floor(Math.random() * colors.length)]
   }
 
   function initProgress ($h1) {
-    var threshold = $h1.offset().top + $h1.height() - $('nav').height(),
-        $postContent = $('.post-content'),
-        ph = $postContent.height(),       // post height
-        wh = $(window).height(),          // window height
-        color = randomColor();
+    var threshold = $h1.offset().top + $h1.height() - $('nav').height()
+    var $postContent = $('.post-content')
+    var ph = $postContent.height()       // post height
+    var wh = $(window).height()          // window height
+    var color = randomColor()
 
     $(document).scroll(function () {
-      if (!$postContent.length) return;
+      if (!$postContent.length) return
 
-      var offsetTop = $postContent.offset().top,
-          y = $(document).scrollTop(),
-          base = Math.max(5, offsetTop + ph - wh),
-          progress = Math.min(100, y / base * 100);
+      var offsetTop = $postContent.offset().top
+      var y = $(document).scrollTop()
+      var base = Math.max(5, offsetTop + ph - wh)
+      var progress = Math.min(100, y / base * 100)
+
       $('#bar').css({
         'width': progress + '%',
         'background-color': color
-      });
+      })
 
       if (y <= threshold) {
-        $('#progress').height('8px');
+        $('#progress').height('8px')
       } else {
-        $('#progress').height('36px');
+        $('#progress').height('36px')
       }
-    });
+    })
   }
 
-  var $h1 = $('h1');
+  var $h1 = $('h1')
   if ($h1.length) {
-    initProgress($h1);
+    initProgress($h1)
   }
 
   $('[data-toggle=collapse]').on('click', function (e) {
-    var target = $(this).attr('data-target');
-    $(target).toggleClass('collapsed');
-  });
-}(jQuery));
+    var target = $(this).attr('data-target')
+    $(target).toggleClass('collapsed')
+  })
+}(jQuery))
